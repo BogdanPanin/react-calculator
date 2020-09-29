982783c368ee3ea7ae044367d6a46db984662ba8
Your Name <bogdan.panin@gmail.com>
2020-09-29 17:32:56 +0300

    Updates

---
+++ precache-manifest.1c3506b707ae377e232cb4216f9dd824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6661662039ddeec55033cd4709772a6a",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "0fc892f010f80fecfffe",
+    "url": "/react-calculator/static/css/main.f34088f4.chunk.css"
+  },
+  {
+    "revision": "1dd6105a7a7febe96f2f",
+    "url": "/react-calculator/static/js/2.f881804c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-calculator/static/js/2.f881804c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc892f010f80fecfffe",
+    "url": "/react-calculator/static/js/main.99c8240a.chunk.js"
+  },
+  {
+    "revision": "14453d16db6c0e44d6da",
+    "url": "/react-calculator/static/js/runtime-main.7b47f775.js"
+  }
+]);
